dd89c0f80e3f629f1cec367a6c546c25fde56131
2011Karthik <100004622+2011Karthik@users.noreply.github.com>
2023-07-12 15:50:46 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 function setup(){
-    
+
+}
+
+function redirect(){
+    window.location = "tv.html";
+}
+
+function redirect2(){
+    window.location = "speaker.html";
+}
+
+function redirect3(){
+    window.location = "fan.html";
+}
+
+function redirect4(){
+    window.location = "toy.html";
+}
+
+function redirect5(){
+    window.location = "microwave.html";
 }
